46b1684a875b43ddc7077b68027311dacc5aa2b4
Angélica <benavidescabezas.angelica@gmail.com>
2025-11-20 11:47:54 -0500

    conexión de base de datos

--- Tkinter.py/Caso de uso2.py
+++ Tkinter.py/Caso de uso2.py
@@ -1,14 +1,4 @@
-import tkinter as tk
-class Presupuesto():
-    def __init__(self):
-        self.ventana=tk.Tk()
-        self.ventana.title("Presupuesto")
-        self.ventana.geometry("500x300")
-        self.ventana.config(bg="lightblue")
-        tk.Label(self.ventana,text="Decoración").place(relx=0.05,rely=0.05,relwidth=0.20,height=0.20)
-        self.ventana.decoracion=tk.Entry(self.ventana)
-        self.ventana.decoracion.pack()
-        self.ventana.mainloop()
+from tkinter import Tk, Label, Button
 
-Presupuesto()
-
+ventana = Tk()
+ventana.title("")
